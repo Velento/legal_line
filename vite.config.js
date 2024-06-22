cbe40833ad1989308ea372662243abe240ce3a77
Velento_Andrei <goggopizza.velento@gmail.com>
2024-06-22 06:18:48 +0300

    Deploy project to GitHub Pages#10

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-vite-website/', // Обновленный базовый путь
-  build: {
-    rollupOptions: {
-      input: 'index.html' // Путь к index.html относительно папки legal_line
-    }
-  }
+  base: '/my-vite-website/',
 });
